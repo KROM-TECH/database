ed4ea636f02cdb62b1b6b10d5dfd1746fd6feca3
kromate <akpananthony33@gmail.com>
2020-05-19 16:40:19 +0100

    firebase_config

---
+++ js/app.js
@@ -0,0 +1,15 @@
+document.getElementById('sub').addEventListener('click', function(e){
+  e.preventDefault();
+
+
+  const university = document.getElementById('university').value;
+  const service = document.getElementById('service').value;
+
+  console.log(university)
+
+
+
+db.collection("").doc("").set(
+  
+);
+ })
---
+++ js/fire.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyD99tTPFLh-pI4v1kbpdkP9x2FS92uVwSo",
+  authDomain: "database-369d0.firebaseapp.com",
+  databaseURL: "https://database-369d0.firebaseio.com",
+  projectId: "database-369d0",
+  storageBucket: "database-369d0.appspot.com",
+  messagingSenderId: "572557648865",
+  appId: "1:572557648865:web:e80c73baae844ad339a8a0",
+  measurementId: "G-M7F2GVNYD8"
+};
